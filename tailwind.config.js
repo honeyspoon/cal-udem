b4389847815bf30679b13009c99ba0ea6ae6067d
Abderahmane Bouziane <bobmatt911@gmail.com>
2023-09-10 15:39:13 -0400

    idem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+const { nextui } = require('@nextui-org/react');
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
   darkMode: 'class',
+  plugins: [nextui()],
 };
