95117e0cebd02726f1f3ec69a2ae8e89a5428e93
Abderahmane Bouziane <bobmatt911@gmail.com>
2023-09-11 11:14:11 -0400

    fix build

--- app/layout-client.js
+++ app/layout-client.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
-
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from '@nextui-org/react';
 import { Providers } from 'app/providers';
+
 export function LayoutClient({ children }) {
   return (
     <>
